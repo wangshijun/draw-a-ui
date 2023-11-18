a275c0e976636d55c67caec01c6e70ae92b1bb62
wangshijun <wangshijun2010@gmail.com>
2023-11-19 07:34:13 +0800

    chore: bundle as a blocklet

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const whenDev = process.env.NODE_ENV === 'development';
+const getDevNextConfig = () => {
+	let mountPoint = process.env.BLOCKLET_DEV_MOUNT_POINT || '';
 
-module.exports = nextConfig
+	let appUrl = process.env.BLOCKLET_APP_URL || '';
+
+	let assetPrefix = `${appUrl}${mountPoint}`.replace('http://', 'https://');
+
+	let devWithBlockletServer = process.env.hasOwnProperty('BLOCKLET_DEV_MOUNT_POINT');
+
+	return whenDev && devWithBlockletServer
+		? {
+			assetPrefix: assetPrefix, // When the dev mode as component, this line required
+			basePath: '',
+		}
+		: {};
+};
+
+const nextConfig = {
+	reactStrictMode: false,
+	...getDevNextConfig(),
+};
+
+/** @type {import('next').NextConfig} */
+module.exports = nextConfig;
